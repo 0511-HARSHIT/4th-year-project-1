61ebfe229df46d72a41414efb794304bcac1f31f
0511-HARSHIT <harshit.yadav26041997@gmail.com>
2026-04-08 21:58:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -54,5 +55,8 @@ def home():
     )
 
 
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
